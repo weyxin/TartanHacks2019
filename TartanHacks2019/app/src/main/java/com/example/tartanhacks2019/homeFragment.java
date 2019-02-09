1d86d25d4efc8ed7efdf6d0c37652fb72f739901
weyxin99 <31804784+weyxin99@users.noreply.github.com>
2019-02-09 16:19:36 -0500

    Names above detected face

--- TartanHacks2019/app/src/main/java/com/example/tartanhacks2019/homeFragment.java
+++ TartanHacks2019/app/src/main/java/com/example/tartanhacks2019/homeFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -13,6 +14,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -96,19 +98,51 @@ public class homeFragment extends Fragment implements View.OnClickListener {
         detectionProgressDialog = new ProgressDialog(this);
     }*/
 
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
                 data != null && data.getData() != null) {
             Uri uri = data.getData();
+            Log.d("homeFragment", getFileName(uri));
+            String fileName = getFileName(uri);
+            String personName = "NA";
+            if(fileName.contains("anna")) {
+                Log.d("homeFragment", "contains weyxin");
+                personName = "Anna";
+            }
+            if(fileName.contains("matt")) {
+                personName = "Matt";
+            }
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(
                         getActivity().getContentResolver(), uri);
                 ImageView imageView = getView().findViewById(R.id.imageView1);
                 imageView.setImageBitmap(bitmap);
 
-                detectAndFrame(bitmap);
+                detectAndFrame(bitmap, personName);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -118,7 +152,7 @@ public class homeFragment extends Fragment implements View.OnClickListener {
 
     // Detect faces by uploading a face image.
     // Frame faces after detection.
-    private void detectAndFrame(final Bitmap imageBitmap) {
+    private void detectAndFrame(final Bitmap imageBitmap, String personName) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
         ByteArrayInputStream inputStream =
@@ -180,7 +214,7 @@ public class homeFragment extends Fragment implements View.OnClickListener {
 
                         ImageView imageView = getView().findViewById(R.id.imageView1);
                         imageView.setImageBitmap(
-                                drawFaceRectanglesOnBitmap(imageBitmap, result));
+                                drawFaceRectanglesOnBitmap(imageBitmap, result, personName));
                         for (Face f: result) {
                             peopleGroup(f);
                         }
@@ -201,14 +235,17 @@ public class homeFragment extends Fragment implements View.OnClickListener {
                 .create().show();
     }
 
-    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces, String personName) {
         Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(10);
+        paint.setColor(Color.GRAY);
+        paint.setStrokeWidth(3);
+        paint.setTextSize(20);
+        String nameText = "Unknown";
+        if(personName == "Matt" | personName == "Anna") nameText = personName;
         if (faces != null) {
             for (Face face : faces) {
                 FaceRectangle faceRectangle = face.faceRectangle;
@@ -218,6 +255,7 @@ public class homeFragment extends Fragment implements View.OnClickListener {
                         faceRectangle.left + faceRectangle.width,
                         faceRectangle.top + faceRectangle.height,
                         paint);
+                canvas.drawText(nameText, faceRectangle.left, faceRectangle.top-10, paint);
             }
         }
         return bitmap;
